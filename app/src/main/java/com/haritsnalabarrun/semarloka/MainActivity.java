1e270659418dc813c6d040c841c47501cf42458d
haritsnb <haritsnalabarrun@gmail.com>
2017-12-29 00:14:49 +0700

    Semarloka
    Tugas Akhir Pemrograman Mobile

---
+++ app/src/main/java/com/haritsnalabarrun/semarloka/MainActivity.java
@@ -0,0 +1,53 @@
+package com.haritsnalabarrun.semarloka;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private BottomNavigationView bottomNavigation;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigation.inflateMenu(R.menu.bottom_navigation_items);
+        fragmentManager = getSupportFragmentManager();
+
+        //Untuk inisialisasi fragment pertama kali
+        fragmentManager.beginTransaction().replace(R.id.main_container, new FragmentWisata()).commit();
+
+        //Memberikan listener saat menu item di bottom navigation diklik
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.menu_wisata:
+                        fragment = new FragmentWisata();
+                        break;
+                    case R.id.menu_tambah:
+                        fragment = new FragmentTambah();
+                        break;
+                    case R.id.menu_profil:
+                        fragment = new FragmentProfil();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+        });
+    }
+}
